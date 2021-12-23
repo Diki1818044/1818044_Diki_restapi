c66f865b2ac518cb73aeb72898797817df8b872b
Diki1818044 <73088709+Diki1818044@users.noreply.github.com>
2021-12-23 09:14:31 +0700

    Update Kamera.php

--- application/controllers/Kamera.php
+++ application/controllers/Kamera.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Mahasiswa extends CI_Controller 
+class Kamera extends CI_Controller 
 {
     function __construct(){
         parent::__construct();
